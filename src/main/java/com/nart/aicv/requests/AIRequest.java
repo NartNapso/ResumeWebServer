1596fde2ce510d38ea2139bbd6d773cdbca92ae5
NartNapso <theelephantlove@gmail.com>
2025-01-09 19:43:28 +0200

    Initial commit for Server

---
+++ src/main/java/com/nart/aicv/requests/AIRequest.java
@@ -0,0 +1,13 @@
+package com.nart.aicv.requests;
+
+public class AIRequest {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
